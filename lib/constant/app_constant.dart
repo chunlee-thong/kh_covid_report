ad3145dce41054f16d81ccc59453bce2b57f153e
chunleethong <thong.chunlee@optistech.com>
2020-07-16 09:20:07 +0700

    Add GoogleMap key for publish use and release 1.0.1

--- lib/constant/app_constant.dart
+++ lib/constant/app_constant.dart
@@ -1,4 +1,4 @@
 class AppConstant {
   static const String BASE_URL = "https://covid19cambodia.herokuapp.com";
-  static const String APP_VERSION = "1.0.0";
+  static const String APP_VERSION = "1.0.1";
 }
