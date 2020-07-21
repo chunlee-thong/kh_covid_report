6221222fa53c4379b37998b41aa96b6d7c071082
Chunlee Thong <chunlee.thong@gmail.com>
2020-07-21 22:24:51 +0700

    update

--- lib/constant/resource_path.dart
+++ lib/constant/resource_path.dart
@@ -1,13 +1,13 @@
-class R{
- static Images images = Images();
+class R {
+  static Images images = Images();
 }
+
 class Images {
-String appicon = "assets/images/app-icon.png";
+  String appicon = "assets/images/app-icon.png";
 
-String medical_care = "assets/images/medical_care.png";
+  String medical_care = "assets/images/medical_care.png";
 
-String medical_report = "assets/images/medical_report.png";
+  String medical_report = "assets/images/medical_report.png";
 
-String pin = "assets/images/pin.png";
-
+  String pin = "assets/images/pin.png";
 }
